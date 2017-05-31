3c8af88be88d77152dae9882c47bbd932abe50e5
dushy <dushy@Dush-PC>
2017-05-31 16:35:27 -0400

    adding test for GetLargestNumberForGivenDigits

--- Project Euler/src/org/dushyant/EulerTest.java
+++ Project Euler/src/org/dushyant/EulerTest.java
@@ -15,6 +15,11 @@ public class EulerTest {
 		assertEquals("total 4 prime numbers below 10 ", "[0, 1, 2, 3, 5, 7]", EulerMath.getPrimeNumbersBelowGivenNumber(10));
 	}
 	
+	@Test
+	public void testGetLargestNumberForGivenDigits_positiveCases() {
+		assertEquals("the largest 2 digits number is 99 ", 99, EulerMath.getLargestNumberForGivenDigits(2));
+		assertEquals("the largest 3 digits number is 999 ", 999, EulerMath.getLargestNumberForGivenDigits(3));
+	}
 	
 
 
